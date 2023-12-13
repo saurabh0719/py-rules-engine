f15198519733ccd656755aeba2c4ce8feedd0de5
Saurabh Pujari <saurabhpuj99@gmail.com>
2023-12-14 03:30:41 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+from py_rules.__version__ import __version__
+
+setup(
+    name="py-rules",
+    packages=["py_rules"],
+    version=__version__,
+    license="three-clause BSD",
+    description=
+    "py-rules is a simple, yet powerful rules engine written in pure Python. It allows you to define complex logical conditions and actions, either via a json file or through pythonic functions.",
+    long_description="Please visit https://github.com/saurabh0719/py-rules#README for detailed documentation!",
+    long_description_content_type='text/plain',
+    author="Saurabh Pujari",
+    author_email="saurabhpuj99@gmail.com",
+    url="https://github.com/saurabh0719/py-rules",
+    keywords=[
+        "rules engine",
+        "rules",
+        "rule evaluator"
+        "business rules",
+    ],
+    project_urls={
+        "Documentation": "https://github.com/saurabh0719/py-rules#README",
+        "Source": "https://github.com/saurabh0719/py-rules",
+    },
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+    ],
+)
