46ce6ea4df99b4f48622822192c1c8585ee6c896
Saurabh Pujari <saurabhpuj99@gmail.com>
2023-12-15 04:29:48 +0530

    update driver code example

--- examples/driver.py
+++ examples/driver.py
@@ -1,4 +1,5 @@
 from py_rules.components import Condition, Result, Rule
+from py_rules.engine import RuleEngine
 from py_rules.storages import JSONRuleStorage, PickledRuleStorage, YAMLRuleStorage
 
 # Define conditions
@@ -27,141 +28,41 @@ YAMLRuleStorage('rule.yaml').store(complex_rule)
 PickledRuleStorage('rule.pkl').store(complex_rule)
 
 # Load the complex rule from each format
-json_rule_loaded = JSONRuleStorage('rule.json').load()
-yaml_rule_loaded = YAMLRuleStorage('rule.yaml').load()
-pickle_rule_loaded = PickledRuleStorage('rule.pkl').load()
+json_rule = JSONRuleStorage('rule.json').load()
+yaml_rule = YAMLRuleStorage('rule.yaml').load()
+pickle_rule = PickledRuleStorage('rule.pkl').load()
 
 # Assert that the loaded rules are equal to the original rule
-assert complex_rule.to_dict() == json_rule_loaded.to_dict() == yaml_rule_loaded.to_dict() == pickle_rule_loaded.to_dict(
-)
+# assert complex_rule == json_rule #== yaml_rule == pickle_rule
 
 # Print the dictionary representation of each rule
 print("\nOriginal rule:", complex_rule.to_dict())
-print("\nJSON loaded rule:", json_rule_loaded.to_dict())
-print("\nYAML loaded rule:", yaml_rule_loaded.to_dict())
-print("\nPickle loaded rule:", pickle_rule_loaded.to_dict())
-assert complex_rule.to_dict() == {
-    "metadata": {
-        "version": "0.1.0",
-        "type": "Rule",
-        "id": "cfb77a2b-a05d-4f9d-be9d-ce442c51b624",
-        "created": "2023-12-14 21:27:01.828293",
-        "required_context_parameters": [],
-        "name": "Complex rule",
-        "parent_id": None,
-    },
-    "if": {
-        "and": [
-            {
-                "condition": {
-                    "metadata": {
-                        "version": "0.1.0",
-                        "type": "Condition",
-                        "id": "3173872f-404d-467b-aef3-507d28a5f53d",
-                        "created": "2023-12-14 21:27:01.828222",
-                        "required_context_parameters": ["number"],
-                    },
-                    "variable": "number",
-                    "operator": "in",
-                    "value": {
-                        "type":
-                            "list",
-                        "value": [
-                            {
-                                "type": "int",
-                                "value": 1
-                            },
-                            {
-                                "type": "int",
-                                "value": 2
-                            },
-                            {
-                                "type": "int",
-                                "value": 3
-                            },
-                        ],
-                    },
-                }
-            },
-            {
-                "condition": {
-                    "metadata": {
-                        "version": "0.1.0",
-                        "type": "Condition",
-                        "id": "b31483ed-e103-474f-b04e-70b2aad71beb",
-                        "created": "2023-12-14 21:27:01.828252",
-                        "required_context_parameters": ["number"],
-                    },
-                    "variable": "number",
-                    "operator": "=",
-                    "value": {
-                        "type": "int",
-                        "value": 1
-                    },
-                }
-            },
-        ]
-    },
-    "then": {
-        "result": {
-            "xyz": {
-                "type": "str",
-                "value": "Condition met"
-            },
-            "result": {
-                "type": "variable",
-                "value": "xyz"
-            },
-        }
-    },
-    "else": {
-        "metadata": {
-            "version": "0.1.0",
-            "type": "Rule",
-            "id": "00757c4b-cae1-4b9c-8480-927a48bcc4a8",
-            "created": "2023-12-14 21:27:01.828282",
-            "required_context_parameters": [],
-            "name": "Nested rule",
-            "parent_id": None,
-        },
-        "if": {
-            "condition": {
-                "metadata": {
-                    "version": "0.1.0",
-                    "type": "Condition",
-                    "id": "3173872f-404d-467b-aef3-507d28a5f53d",
-                    "created": "2023-12-14 21:27:01.828222",
-                    "required_context_parameters": ["number"],
-                },
-                "variable": "number",
-                "operator": "in",
-                "value": {
-                    "type": "list",
-                    "value": [
-                        {
-                            "type": "int",
-                            "value": 1
-                        },
-                        {
-                            "type": "int",
-                            "value": 2
-                        },
-                        {
-                            "type": "int",
-                            "value": 3
-                        },
-                    ],
-                },
-            }
-        },
-        "then": {
-            "result": {
-                "xyz": {
-                    "type": "str",
-                    "value": "Condition met"
-                }
-            }
-        },
-        "else": None,
-    },
-}
+print("\nJSON loaded rule:", json_rule.to_dict())
+assert complex_rule == json_rule  #== yaml_rule == pickle_rule
+print("\nYAML loaded rule:", yaml_rule.to_dict())
+print("\nPickle loaded rule:", pickle_rule.to_dict())
+
+# Hide metadata in the dictionary representation of the rule
+complex_rule.kwargs['hide_metadata'] = True
+complex_rule.load_metadata()
+print("\nComplex rule - ", complex_rule.to_dict())
+print("\nJSON rule - ", json_rule.to_dict())
+assert complex_rule != json_rule
+
+complex_rule.kwargs['hide_metadata'] = False
+complex_rule.load_metadata()
+print("\nComplex rule - ", complex_rule.to_dict())
+print("\nJSON rule - ", json_rule.to_dict())
+assert complex_rule == json_rule
+
+# Create a RuleEngine and add the complex rule
+context = {'number': 1, 'xyz': 5}  # Define a context
+engine = RuleEngine(context)
+
+# Evaluate the rules against the contexts
+print("\nEvaluation of context1:", engine.evaluate(complex_rule))
+print("\nEvaluation of context1 with JSON rule:", engine.evaluate(json_rule))
+
+context = {'number': 4, 'xyz': 5}  # Update the context
+engine = RuleEngine(context)  # Create a new RuleEngine with the updated context
+print("\nEvaluation of context2:", engine.evaluate(complex_rule))
